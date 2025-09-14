6032057440f46eeec0ca9200f75fbc8cf33fc03f
lavenderses <nakanoinryota@gmail.com>
2025-09-15 06:15:09 +0900

    Revert "Initialize simple popup"
    This reverts commit f39b81ea1ce4d47891d7836abe517b6377365a63.

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite'
-import { join, resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: resolve(__dirname, 'src'),
-  publicDir: resolve(__dirname, 'public'),
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    rollupOptions: {
-      input: {
-        popup: join(__dirname, 'src/popup/index.html'),
-      },
-      output: {
-        entryFileNames: 'assets/[name].js'
-      },
-    }
-  },
-  resolve: {
-    alias: {
-      '@src': resolve(__dirname, 'src')
-    }
-  },
   plugins: [react()],
 })
