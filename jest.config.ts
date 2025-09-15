8a38e752128c511d66ba53f9c71ba5ca9386844a
lavenderses <nakanoinryota@gmail.com>
2025-09-15 11:39:09 +0900

    Jest env

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageReporters: ['text'],
+};
+
+export default config;
